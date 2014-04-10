a629333919a9f669aac6b49bcc3de2c970aa602d
agrodecki <agrodecki@bravurasolutions.com>
2014-04-10 14:41:18 +0200

    generated ID value added

--- ArtTel/src/org/arttel/entity/Order.java
+++ ArtTel/src/org/arttel/entity/Order.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +13,7 @@ import javax.persistence.Table;
 public class Order {
 
 	@Id
+	@GeneratedValue
 	@Column(name="OrderId")
 	private Integer orderId;
 
